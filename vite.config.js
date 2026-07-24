af92e3a1162bc97da8819c1299eea1962eed601c
rajgupta04 <engineerrajgupta@gmail.com>
2026-07-24 13:29:15 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/portfolio/",
+  base: "/rajgupta/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
