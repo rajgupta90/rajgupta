5d4520999fdaa1159a9f756e04bd696f5000167c
Raj Gupta <rajgupta23@lpu.in>
2026-07-24 13:34:37 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/rajgupta/",
+  base: ".",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
