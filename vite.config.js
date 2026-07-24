dafd23e87ab7e51efaee210041147a3a5b30ff46
Raj Gupta <rajgupta23@lpu.in>
2026-07-24 13:47:36 +0530

    Update path resolution in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-
   plugins: [tailwindcss(), react()],
   base: "/rajgupta/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
-
